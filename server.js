3a4261c3a84cbabdc91e96427d4fbb5118ac4603
Amela Kurtic <amela.kurtic@student.tugraz.at>
2020-11-17 15:12:46 +0100

    malo sam glupa

--- server.js
+++ server.js
@@ -44,6 +44,35 @@ server.on('listening', () => {
     console.log('Listening on ' + bind)
 })
 
+app.post("/upload", function(request, response) {
+    var images = new Array();
+    if(request.files) {
+        var arr;
+        if(Array.isArray(request.files.filesfld)) {
+            arr = request.files.filesfld;
+        }
+        else {
+            arr = new Array(1);
+            arr[0] = request.files.filesfld;
+        }
+        for(var i = 0; i < arr.length; i++) {
+            var file = arr[i];
+            if(file.mimetype.substring(0,5).toLowerCase() == "image") {
+                images[i] = "/" + file.name;
+                file.mv("./public" + images[i], function (err) {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+            }
+        }
+    }
+    // give the server a second to write the files
+    setTimeout(function(){response.json(images);}, 1000);
+});
+
+
+
 
 // Web sockets
 const io = require('socket.io')(server)
